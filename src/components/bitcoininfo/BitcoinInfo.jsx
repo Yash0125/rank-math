4087fddb61197d344c77106becade2d440fe4b33
Yash0125 <116873935+Yash0125@users.noreply.github.com>
2023-07-22 15:42:36 +0530

    fix : style

--- src/components/bitcoininfo/BitcoinInfo.jsx
+++ src/components/bitcoininfo/BitcoinInfo.jsx
@@ -1,6 +1,6 @@
-import React, { useState } from 'react';
-import './bitcoinInfo.css';
-import { BsCurrencyBitcoin } from "react-icons/bs";
+import React, { useState } from "react";
+import "./bitcoinInfo.css";
+import { BiBitcoin } from "react-icons/bi";
 import { SlArrowDown } from "react-icons/sl";
 
 const BitcoinInfo = () => {
@@ -12,33 +12,34 @@ const BitcoinInfo = () => {
 
   return (
     <>
-      <section className='bitcoin-container'>
-        <div className='top-row'>
-          <div className='bitcoin-logo'>
-            <BsCurrencyBitcoin className='logo' />
+      <section className="bitcoin-container">
+        <div className="top-row">
+          <div className="bitcoin-logo">
+            <BiBitcoin className="logo" size={100} />
           </div>
-          <div className='bitcoin'>Bitcoin</div>
-          <div className='btc'>BTC</div>
+
+          <div className="bitcoin">Bitcoin</div>
+          <div className="btc">BTC</div>
         </div>
-        <div className='mid-row'>
+        <div className="mid-row">
           <span>3.529020 BTC</span>
         </div>
-        <div className='last-row'>
-          <span className='bitcoin-price'>$19.153 USD</span>
-          <span className='bicoin-profit'>-2.32%</span>
+        <div className="last-row">
+          <span className="bitcoin-price">$19.153 USD</span>
+          <span className="bicoin-profit">-2.32%</span>
         </div>
-        <div className='dropdown' onClick={handleArrowClick}>
+        <div className="dropdown" onClick={handleArrowClick}>
           <SlArrowDown />
         </div>
         {showButtons && (
-          <div className='buttons-container'>
-            <button className='buy-button'>Buy</button>
-            <button className='sell-button'>Sell</button>
+          <div className="buttons-container">
+            <button className="buy-button">Buy</button>
+            <button className="sell-button">Sell</button>
           </div>
         )}
       </section>
     </>
   );
-}
+};
 
 export default BitcoinInfo;
