2c1c34b1e6b6679e51891bd0c076999cbfe9b9d9
Yash0125 <116873935+Yash0125@users.noreply.github.com>
2023-07-22 14:50:21 +0530

    refactor

--- src/components/bitcointrade/BitcoinTrade.jsx
+++ src/components/bitcointrade/BitcoinTrade.jsx
@@ -1,16 +1,15 @@
-import React from 'react';
-import './bitcoinTrade.css'
-import BitcoinBuy from '../bitcoinbuy/BitcoinBuy';
-import BitcoinSell from '../bitcoinSell/BitcoinSell';
+import React from "react";
+import "./bitcoinTrade.css";
+import BitcoinBuy from "../bitcoinbuy/BitcoinBuy";
+import BitcoinSell from "../bitcoinSell/BitcoinSell";
 
 const BitcoinTrade = () => {
   return (
-    <div className='bitcoinTrade-container'>
-    <BitcoinBuy/>
-    <BitcoinSell/>
-      
+    <div className="bitcoinTrade-container">
+      <BitcoinBuy />
+      <BitcoinSell />
     </div>
-  )
-}
+  );
+};
 
-export default BitcoinTrade
+export default BitcoinTrade;
