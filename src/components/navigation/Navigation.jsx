6832093d9affea643d841526512f8711a3493b8a
Yash0125 <116873935+Yash0125@users.noreply.github.com>
2023-07-22 15:23:36 +0530

    feat : dropmenu added

--- src/components/navigation/Navigation.jsx
+++ src/components/navigation/Navigation.jsx
@@ -1,17 +1,51 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './navigation.css';
 import { GrMoreVertical } from "react-icons/gr";
 import { SlArrowLeft } from "react-icons/sl";
+import { CgRename } from "react-icons/cg";
+import { PiDeviceTabletSpeaker } from "react-icons/pi";
+import { BiArrowFromBottom } from "react-icons/bi";
+import { IoMdRemoveCircleOutline } from "react-icons/io";
 
 const Navigation = () => {
+  const [showMoreIcons, setShowMoreIcons] = useState(false);
+
+  const handleMoreIconClick = () => {
+    setShowMoreIcons(prevState => !prevState);
+  };
+
   return (
     <header className='navigation'>
       <div className='left-icon'>
         <SlArrowLeft />
       </div>
       <h1 className='title'>Bitcoin wallet</h1>
-      <div className='right-icon'>
+      <div className='right-icon' onClick={handleMoreIconClick}>
         <GrMoreVertical />
+      </div>
+      <div className={`more-icons ${showMoreIcons ? 'visible' : 'hidden'}`}>
+        <ul>
+          <li>
+            <span>Edit</span>
+            <CgRename />
+          </li>
+          <hr />
+          <li>
+            <span>Courier info</span>
+            <PiDeviceTabletSpeaker />
+          </li>
+          <hr />
+          <li>
+            <span>Share</span>
+            <BiArrowFromBottom />
+          </li>
+          <hr />
+          <li>
+            <span>Remove</span>
+            <IoMdRemoveCircleOutline />
+          </li>
+          <hr />
+        </ul>
       </div>
     </header>
   );
